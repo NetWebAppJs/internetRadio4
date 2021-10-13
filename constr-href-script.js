9a58db1f700ef8191b3c718747a75fa8e4f8988d
Andrew Berlin <78539122+berlinandrew@users.noreply.github.com>
2021-10-14 01:34:59 +0300

    Add files via upload

---
+++ constr-href-script.js
@@ -0,0 +1,4 @@
+let imgHref = document.getElementById('myImage');
+imgHref.src = 'https://64.media.tumblr.com/f978b298c67707dd2b32c7fb6a0997eb/9260294ef3271a9e-95/s1280x1920/82fa9cf9e2aac980800a94d74716359aeb6e557e.png';
+let imgStyle = document.getElementById("myImage");
+imgStyle.style.width = "70%";
