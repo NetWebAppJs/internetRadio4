b64ddfef28d2317e47df5af99fa46d0ebb2b82c3
Andrew Berlin <78539122+berlinandrew@users.noreply.github.com>
2021-10-17 14:46:49 +0300

    Update constr-href-script.js

--- constr-href-script.js
+++ constr-href-script.js
@@ -1,4 +1,4 @@
 let imgHref = document.getElementById('myImage');
-imgHref.src = 'https://64.media.tumblr.com/f978b298c67707dd2b32c7fb6a0997eb/9260294ef3271a9e-95/s1280x1920/82fa9cf9e2aac980800a94d74716359aeb6e557e.png';
+imgHref.src = 'https://64.media.tumblr.com/f2ce65bef36a3c12324b8721e3d7a2e1/7a0522c95fd9ab9a-ef/s1280x1920/e37a4dbbfadba87e863dfcf778735d62a7d35a52.png';
 let imgStyle = document.getElementById("myImage");
 imgStyle.style.width = "70%";
